6e0536f207f091e12e578bc9e397fce4634f2950
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-08-13 14:25:54 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'ac0c8977-677c-4305-abdf-e043cf696ffa';
+
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height='100vh'
-            projectID='ac0c8977-677c-4305-abdf-e043cf696ffa'
-            userName='d.sharchekev'
-            userSecret='Aminadastan2013'
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             rednerChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
-    )
+    );
 }
 
 export default App;
