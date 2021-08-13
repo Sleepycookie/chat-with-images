41a42171dc67e1ac8f34716fedccf73c6ff6a495
Dastan Sharchekev <47035775+Sleepycookie@users.noreply.github.com>
2021-08-13 13:55:58 +0200

    API calls

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,11 +1,22 @@
-import MessageForm from './MessageForm';
 import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
 
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
 
     const chat = chats && chats[activeChat];
+
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+        <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+                float: isMyMessage ? 'right' : 'left',
+                backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+            }}
+        />
+    ));
 
     const renderMessages = () => {
         const keys = Object.keys(messages);
@@ -23,14 +34,12 @@ const ChatFeed = (props) => {
                             : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
                     </div>
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
-
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
                 </div>
             );
         });
     };
-
-    renderMessages()
 
     if (!chat) return <div />;
 
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+}
